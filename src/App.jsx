5a860e0b6d2e57792a652a34fb77ce76d0ee1ea6
unknown <bhavemona@gmail.com>
2021-03-15 16:47:49 +0530

    aboutus final

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Event from "./Sections/Event/Event";
 import AboutUs from './Sections/AboutUs/AboutUs';
+
 function App() {
   return (
     <>
