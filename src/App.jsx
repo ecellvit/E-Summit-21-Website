9ec18267d466e2d5ec016f681c340bcd101450e7
unknown <bhavemona@gmail.com>
2021-03-15 12:25:34 +0530

    about us added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Event from "./Sections/Event/Event";
-
+import AboutUs from './Sections/AboutUs/AboutUs';
 function App() {
   return (
     <>
+    <AboutUs />
     <Event />
+    
     </>
   );
 }
