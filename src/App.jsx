ee00d54685672bb81888fc836043650d26687cf5
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2021-03-15 20:57:20 +0530

    fix: minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './App.css';
 import Event from "./Sections/Event/Event";
 import AboutUs from './Sections/AboutUs/AboutUs';
-import Landing from "./Sections/Landing/Landing";
+
 function App() {
   return (
     <>
-    {/* <Landing />
-    <AboutUs /> */}
+    <AboutUs />
     <Event />
     
     </>
