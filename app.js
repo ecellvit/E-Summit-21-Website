85322b0395880ef98c187836916d5622de90b62f
unknown <bhavemona@gmail.com>
2021-03-20 20:59:04 +0530

    node added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
 const port = 3000;
 
 app.get("/", (req, res) => {
