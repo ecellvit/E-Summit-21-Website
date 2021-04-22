9413ebc589d14a8f2c27e2ee57a2c77115354982
Vinayak Gupta <guptavinayakg@gmail.com>
2021-04-22 23:01:56 +0530

    basic fix

--- src/Sections/AboutUs/AboutUs.jsx
+++ src/Sections/AboutUs/AboutUs.jsx
@@ -61,12 +61,12 @@ function AboutUs() {
           workshops, exciting expos and spectacular speakers, E-Summit aims at
           instilling innovation in young minds.
         </p>
-        <br />
+        {/* <br />
         <p data-aos="zoom-in" className="aboutus__aboutusinfo">
           E-Summit 2021 is the 4th edition of the biggest business fest in South
           India. It is expected to have a massive turnout of participants, this
           providing a great platform for marketing and outreach.
-        </p>
+        </p> */}
       </div>
     </div>
   );
